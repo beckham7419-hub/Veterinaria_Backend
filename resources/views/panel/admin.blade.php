078d7eda3255750c2015567b490d39ce0a0efdc1
Raphina-core <rafaelmorenov12@gmail.com>
2026-07-24 23:56:21 -0600

    Añadi un poco de estructura a la vista del blade de admin, junto con el nombre del admin mediante el token

--- resources/views/panel/admin.blade.php
+++ resources/views/panel/admin.blade.php
@@ -4,8 +4,63 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Panel del admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
-    <h1>¡Bienvenido administrador!</h1>
-  </body>
+  <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div class="container-fluid">
+    <img src="{{ asset('Imagenes/logo_de_la_veterinaria.jpg') }}" width="50px" height="50px"  style="border-radius: 50px;">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Gestionar personal</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Gestionar inventario de medicamentos</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Mas opciones
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Gestionar citas</li>
+            <li><a class="dropdown-item" href="#">Gestionar clientes</a></li>
+            <li><a class="dropdown-item" href="#">Gestionar mascotas</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<h1 class="text-center mt-4">¡Bienvenido <span id="nombre-usuario">Cargando...</span>!</h1>
+<script>
+  document.addEventListener("DOMContentLoaded", () => {
+   
+    const token = localStorage.getItem('token_veterinaria'); 
+
+    if (token) {
+      try {
+        const payloadBase64 = token.split('.')[1];
+        const payloadDecoded = JSON.parse(atob(payloadBase64));
+
+        if (payloadDecoded.nombre_completo) {
+          document.getElementById('nombre-usuario').innerText = payloadDecoded.nombre_completo;
+        }
+      } catch (e) {
+        console.error("Error al decodificar el token:", e);
+        document.getElementById('nombre-usuario').innerText = " Usuario";
+      }
+    } else {
+      document.getElementById('nombre-usuario').innerText = " Invitado";
+    }
+  });
+</script>
+</body>
 </html>
--- routes/api.php
+++ routes/api.php
@@ -6,27 +6,6 @@ use App\Http\Controllers\AuthUsuarioController;
 use App\Http\Controllers\AuthDuenoController;
 use App\Http\Controllers\PerfilDuenoController;
 
-Route::get('/login-prueba', function () {
-    return view('login');
-});
-
-Route::get('/panel/recepcion', function () {
-    return view('panel.recepcion'); 
-});
-
-Route::get('/panel/consultas', function () {
-    return view('panel.consultas');
-});
-
-Route::get('/panel/admin', function () {
-    return view('panel.admin');
-});
-
-Route::get('/panel/veterinario', function () {
-    return view('panel.veterinario');
-});
-
-
 Route::apiResource('usuarios', UsuarioController::class)->middleware(['auth:usuarios', 'token.valido:usuarios', 'rol:administrador']);
 Route::apiResource('duenos', DuenoController::class)->except(['store'])->middleware(['auth:usuarios', 'token.valido:usuarios', 'rol:recepcionista']);
 Route::post('duenos', [DuenoController::class, 'store']);
